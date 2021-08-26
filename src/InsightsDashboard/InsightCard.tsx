5aade40963d8bfb8d68d544eb045693675e4cf4f
Mina Chuong <mina.chuong@gmail.com>
2021-08-25 19:24:37 -0700

    add thousands formatting on metrics

--- src/InsightsDashboard/InsightCard.tsx
+++ src/InsightsDashboard/InsightCard.tsx
@@ -3,6 +3,15 @@ import "./InsightCard.scss"
 
 export const InsightCard = (props: InsightCardProps) => {
   const { insight } = props;
+  
+  const formatThousands = (amount: number) => {
+    let digits = amount.toString().split("");
+    for(let i=digits.length-3; i>0; i -= 3) {
+      digits.splice(i, 0, ',');
+    }
+
+    return `${digits.join("")}`
+  }
 
   return(
     <article className="tempest-card">
@@ -12,11 +21,11 @@ export const InsightCard = (props: InsightCardProps) => {
         <h4 className="tempest-card__heading--small">{insight.user.occupation}</h4>
       </header>
       <aside className="tempest-card__aside">
-        <p className="tempest-card__aside__metric--orange">{insight.total_impressions}</p>
+        <p className="tempest-card__aside__metric--orange">{formatThousands(insight.total_impressions)}</p>
         <p className="tempest-card__aside__sub-text">impressions</p>
-        <p className="tempest-card__aside__metric--blue">{insight.total_conversions}</p>
+        <p className="tempest-card__aside__metric--blue">{formatThousands(insight.total_conversions)}</p>
         <p className="tempest-card__aside__sub-text">conversions</p>
-        <p className="tempest-card__aside__metric--green--large">{insight.total_revenue}</p>
+        <p className="tempest-card__aside__metric--green--large">{`$${formatThousands(insight.total_revenue)}`}</p>
       </aside>
       <div className="tempest-card__chart">
         {props.children}
--- src/shared/DateFormat.tsx
+++
@@ -1,3 +0,0 @@
-export const DateFormat = (date: string, format: string) => {
-  
-}
